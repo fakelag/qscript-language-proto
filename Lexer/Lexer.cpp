0bfce60bc56f55400e3f60f7e39f91034461f693
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-15 14:09:43 +0300

    Add debug symbols to lexer

--- Lexer/Lexer.cpp
+++ Lexer/Lexer.cpp
@@ -1,14 +1,16 @@
-#pragma once
+#include <exception>
 #include <algorithm>
 #include "Lexer.h"
 
 namespace Lexer
 {
-	std::vector< Grammar::SymbolInfo_t > Parse( const std::string source )
+	std::vector< LexerSymbol_t > Parse( const std::string source )
 	{
-		std::vector< Grammar::SymbolInfo_t > symbols;
+		std::vector< LexerSymbol_t > symbols;
 		std::vector< Grammar::SymbolInfo_t > commonSymbols;
 		std::string stringBuffer = "";
+		int currentLineNr = 0;
+		int currentColNr = 0;
 
 		// Map common symbols to a vector
 		for ( auto it = Grammar::LanguageSymbols.cbegin(); it != Grammar::LanguageSymbols.cend(); ++it )
@@ -23,24 +25,46 @@ namespace Lexer
 		// Get the longest searchable token length
 		int longestToken = commonSymbols.size() > 0 ? commonSymbols[ 0 ].m_Token.length() : 0;
 
+		auto pushSymbol = [ &currentLineNr, &currentColNr, &stringBuffer, &symbols ]() -> void {
+			if ( stringBuffer.length() == 0 )
+				return;
+
+			Grammar::SymbolGroup symbolGroup = Grammar::SG_NAME;
+			try
+			{
+				std::stoi( stringBuffer );
+				symbolGroup = Grammar::SG_INTCONST;
+			}
+			catch ( std::exception e )
+			{
+				// Conversion failed, it's probably a name
+			}
+
+			// Push the last accumulated string as SG_NAME and flush stringBuffer
+			symbols.push_back( {
+				{ stringBuffer, Grammar::LBP_NAME, symbolGroup },
+				{ currentLineNr, currentColNr },
+			} );
+
+			currentColNr += stringBuffer.length();
+			stringBuffer = "";
+		};
+
 		// Iterate through source and look for common symbols
 		for ( auto srcIt = source.cbegin(); srcIt != source.cend(); ++srcIt )
 		{
-			auto findSymbol = [ &commonSymbols, &symbols, &stringBuffer ]( std::string pattern ) -> int {
+			auto findSymbol = [ &commonSymbols, &symbols, &currentLineNr, &currentColNr, &pushSymbol ]( std::string pattern ) -> int {
 				// Iterate all the commons
 				for ( auto symIt = commonSymbols.cbegin(); symIt != commonSymbols.cend(); ++symIt )
 				{
 					if ( symIt->m_Token == pattern.substr( 0, symIt->m_Token.length() ) )
 					{
-						if ( stringBuffer.length() > 0 )
-						{
-							// Push the last accumulated string as SG_NAME and flush stringBuffer
-							symbols.push_back( { stringBuffer, Grammar::LBP_NAME, Grammar::SG_NAME } );
-							stringBuffer = "";
-						}
+						pushSymbol();
 
 						// Push the found common symbol
-						symbols.push_back( *symIt );
+						symbols.push_back( { *symIt, { currentLineNr, currentColNr } } );
+						currentColNr += symIt->m_Token.length();
+
 						return symIt->m_Token.length();
 					}
 				}
@@ -62,32 +86,40 @@ namespace Lexer
 			case '\n':
 			case '\t':
 			case ' ':
-				if ( stringBuffer.length() > 0 )
+			{
+				pushSymbol();
+				if ( *srcIt != '\n' )
 				{
-					// Push the last accumulated string as SG_NAME and flush stringBuffer
-					symbols.push_back( { stringBuffer, Grammar::LBP_NAME, Grammar::SG_NAME } );
-					stringBuffer = "";
+					++currentColNr;
+				}
+				else
+				{
+					currentColNr = 0;
+					++currentLineNr;
 				}
 
 				break;
+			}
 			default:
+			{
 				// Add the character to the symbol buffer
 				stringBuffer += *srcIt;
 				break;
+			}
 			}
 		}
 
 		return symbols;
 	}
 
-	std::string Stringify( const std::vector< Grammar::SymbolInfo_t >& symbols )
+	std::string Stringify( const std::vector< LexerSymbol_t >& symbols )
 	{
 		std::string output;
 
 		char szFormat[ 256 ];
 		for ( auto symbol : symbols )
 		{
-			std::snprintf( szFormat, sizeof( szFormat ), "%s | lbp: %i\n", symbol.m_Token.c_str(), symbol.m_LBP );
+			std::snprintf( szFormat, sizeof( szFormat ), "%s | lbp: %i\n", symbol.m_SymbolInfo.m_Token.c_str(), symbol.m_SymbolInfo.m_LBP );
 			output += szFormat;
 		}
 
