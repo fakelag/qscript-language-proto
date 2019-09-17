f97d876d2aa6c301850a14969abe6d55bd597671
Samuel <samuel@RNDs-MacBook-Pro-2.local>
2019-09-17 21:50:24 +0300

    Add FORCEINLINE define to fill __forceinline on non MSVC compilers

--- Includes/Value.h
+++ Includes/Value.h
@@ -2,6 +2,12 @@
 
 #include <string>
 
+#if defined(_WIN32)
+#define FORCEINLINE FORCEINLINE
+#elif defined(_OSX)
+#define FORCEINLINE inline
+#endif
+
 namespace Value
 {
 	enum ValueType
@@ -20,23 +26,24 @@ namespace Value
 		CValue( double decimal );
 		CValue( bool boolean );
 
-		__forceinline void					SetString( const std::string& string )		{ m_StringValue = string; m_ValueType = VT_STRING; };
-		__forceinline void					SetInt( int integer )						{ m_IntValue = integer; m_ValueType = VT_INTEGER; };
-		__forceinline void					SetDouble( double decimal )					{ m_DoubleValue = decimal; m_ValueType = VT_DOUBLE; };
-		__forceinline void					SetBool( bool boolean )						{ SetInt( boolean ? 1 : 0 ); };
+		FORCEINLINE void					SetString( const std::string& string )		{ m_StringValue = string; m_ValueType = VT_STRING; };
+		FORCEINLINE void					SetInt( int integer )						{ m_IntValue = integer; m_ValueType = VT_INTEGER; };
+		FORCEINLINE void					SetDouble( double decimal )					{ m_DoubleValue = decimal; m_ValueType = VT_DOUBLE; };
+		FORCEINLINE void					SetBool( bool boolean )						{ SetInt( boolean ? 1 : 0 ); };
 
 		// TODO: Make conversions in runtime from one type to another. E.g interger 10 to string "10".
 		// Perhaps have a "Fast" version of the function and a converting one
-		__forceinline const std::string&	GetString()			const { return m_StringValue; };
-		__forceinline int					GetInt()			const { return m_IntValue; };
-		__forceinline double				GetDouble()			const { return m_DoubleValue; };
-		__forceinline bool					GetBool()			const { return !!GetInt(); };
-		__forceinline ValueType				GetType()			const { return m_ValueType; };
+		FORCEINLINE const std::string&	GetString()			const { return m_StringValue; };
+		FORCEINLINE int					GetInt()			const { return m_IntValue; };
+		FORCEINLINE double				GetDouble()			const { return m_DoubleValue; };
+		FORCEINLINE bool				GetBool()			const { return !!GetInt(); };
+		FORCEINLINE ValueType			GetType()			const { return m_ValueType; };
+
 
 	private:
-		std::string		m_StringValue;
-		int				m_IntValue;
-		double			m_DoubleValue;
-		ValueType		m_ValueType;
+		std::string			m_StringValue;
+		int					m_IntValue;
+		double				m_DoubleValue;
+		enum ValueType		m_ValueType;
 	};
 }
