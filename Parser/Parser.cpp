5a19fa367c3c942a32e7fd7398c439d777e4da96
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-18 20:36:19 +0300

    Add Parser::Stringify logic

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -2,8 +2,20 @@
 
 namespace Parser
 {
-	std::vector< AST::IExpression > Parse( const std::vector< Lexer::LexerSymbol_t >& symbols )
+	std::vector< AST::IExpression* > Parse( const std::vector< Lexer::LexerSymbol_t >& symbols )
 	{
+		std::vector< AST::IExpression* > expressions;
 
+		return expressions;
+	}
+
+	std::string Stringify( const std::vector< AST::IExpression* >& expressions )
+	{
+		std::string output;
+
+		for (auto expression : expressions)
+			output += expression->ToString();
+
+		return output;
 	}
 }
