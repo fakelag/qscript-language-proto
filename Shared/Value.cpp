e44e55510a137793fd1489b170806f14397352e8
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-18 20:36:44 +0300

    Add empty + copy constructor to CValue

--- Shared/Value.cpp
+++ Shared/Value.cpp
@@ -2,6 +2,21 @@
 
 namespace Value
 {
+	CValue::CValue()
+	{
+		m_DoubleValue = 0.0;
+		m_IntValue = 0;
+		m_ValueType = VT_INTEGER;
+	}
+
+	CValue::CValue( const CValue& value )
+	{
+		m_ValueType = value.m_ValueType;
+		m_DoubleValue = value.m_DoubleValue;
+		m_IntValue = value.m_IntValue;
+		m_StringValue = value.m_StringValue;
+	}
+
 	CValue::CValue( const std::string& string )
 	{
 		SetString( string );
