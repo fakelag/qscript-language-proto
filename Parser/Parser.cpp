df7d3fad60be3f99889b413b87b5721f96a1ca4f
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-19 19:17:01 +0300

    Add initial parser code (WIP)

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -1,12 +1,126 @@
+#include <functional>
 #include "Parser.h"
 
 namespace Parser
 {
+	struct ParserSymbol_t;
+
+	using LeftBindFn = std::function<AST::IExpression*(const ParserSymbol_t& symbol, AST::IExpression* left)>;
+	using RightBindFn = std::function<AST::IExpression*(const ParserSymbol_t& symbol)>;
+
+	struct ParserSymbol_t
+	{
+		Lexer::LexerSymbol_t 	m_LexerSymbol;
+		LeftBindFn				m_LeftBind;
+		RightBindFn				m_RightBind;
+	};
+
+	class CParserState
+	{
+	public:
+		CParserState()
+		{
+			m_CurrentSymbol = 0;
+		}
+
+		void AddSymbols( const std::vector< ParserSymbol_t >& symbols )
+		{
+			m_Symbols = symbols;
+		}
+
+		bool IsFinished()
+		{
+			return m_CurrentSymbol >= m_Symbols.size() - 1;
+		}
+
+		const ParserSymbol_t& CurrentSymbol()
+		{
+			if (IsFinished())
+				throw "Reading tokens past end of stream";
+
+			return m_Symbols[m_CurrentSymbol];
+		}
+
+		// Increments the current symbol index
+		// & returns the previous symbol
+		const ParserSymbol_t& NextSymbol()
+		{
+			if (IsFinished())
+				throw "Reading tokens past end of stream";
+
+			return m_Symbols[++m_CurrentSymbol - 1];
+		}
+
+		void AddExpression( AST::IExpression* expression )
+		{
+			m_Expressions.push_back( expression );
+		}
+
+		const std::vector< AST::IExpression* >& Expressions()
+		{
+			return m_Expressions;
+		}
+
+	private:
+		std::vector< AST::IExpression* > 			m_Expressions;
+		std::vector< ParserSymbol_t >				m_Symbols;
+		int 										m_CurrentSymbol;
+	};
+
 	std::vector< AST::IExpression* > Parse( const std::vector< Lexer::LexerSymbol_t >& symbols )
 	{
-		std::vector< AST::IExpression* > expressions;
+		CParserState parserState;
 
-		return expressions;
+		auto nextExpression = [&parserState](int rbp = 0) -> AST::IExpression*
+		{
+			auto curSymbol = parserState.NextSymbol();
+			auto leftSide = curSymbol.m_RightBind(curSymbol);
+
+			if (rbp == -1)
+				return leftSide;
+
+			while (rbp < curSymbol.m_LexerSymbol.m_SymbolInfo.m_LBP)
+			{
+				auto nextSymbol = parserState.NextSymbol();
+				leftSide = nextSymbol.m_RightBind(nextSymbol);
+			}
+
+			return leftSide;
+		};
+
+		std::vector< ParserSymbol_t > parserSymbols;
+		std::transform(symbols.begin(), symbols.end(), parserSymbols.begin(),
+			[&nextExpression]( const Lexer::LexerSymbol_t& lexerSymbol ) -> ParserSymbol_t {
+				ParserSymbol_t symbol;
+				symbol.m_LexerSymbol = lexerSymbol;
+				symbol.m_LeftBind = NULL;
+				symbol.m_RightBind = NULL;
+
+				switch (lexerSymbol.m_Symbol)
+				{
+					case Grammar::Symbol::S_ADD:
+					{
+						symbol.m_LeftBind = [&nextExpression](const ParserSymbol_t& symbol, AST::IExpression* left) -> AST::IExpression*
+						{
+							auto right = nextExpression(symbol.m_LexerSymbol.m_SymbolInfo.m_LBP);
+							auto assignExpr = new AST::CComplexExpression( left, right, symbol.m_LexerSymbol.m_Symbol, symbol.m_LexerSymbol.m_DebugInfo );
+							return assignExpr;
+						};
+						break;
+					}
+					default:
+						throw "Unhandled symbol";
+				}
+
+				return symbol;
+			});
+
+		while (!parserState.IsFinished())
+		{
+			auto currentSymbol = parserState.NextSymbol();
+		}
+
+		return parserState.Expressions();
 	}
 
 	std::string Stringify( const std::vector< AST::IExpression* >& expressions )
