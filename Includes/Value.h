bf3a327a6383d7ded2cc7f9d85c24a593b0ace57
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-21 12:57:10 +0300

    Add internal value conversions to CValue (+caching)

--- Includes/Value.h
+++ Includes/Value.h
@@ -3,7 +3,7 @@
 #include <string>
 
 #if defined(_WIN32)
-#define FORCEINLINE FORCEINLINE
+#define FORCEINLINE __forceinline
 #elif defined(_OSX)
 #define FORCEINLINE inline
 #endif
@@ -28,34 +28,32 @@ namespace Value
 		CValue( double decimal );
 		CValue( bool boolean );
 
-		FORCEINLINE void					SetString( const std::string& string )		{ m_StringValue = string; m_ValueType = VT_STRING; };
-		FORCEINLINE void					SetInt( int integer )						{ m_IntValue = integer; m_ValueType = VT_INTEGER; };
-		FORCEINLINE void					SetDouble( double decimal )					{ m_DoubleValue = decimal; m_ValueType = VT_DOUBLE; };
-		FORCEINLINE void					SetBool( bool boolean )						{ SetInt( boolean ? 1 : 0 ); };
+		FORCEINLINE void					SetString( const std::string& string ) { m_StringValue = string; m_ValueType = VT_STRING; };
+		FORCEINLINE void					SetInt( int integer ) { m_IntValue = integer; m_ValueType = VT_INTEGER; };
+		FORCEINLINE void					SetDouble( double decimal ) { m_DoubleValue = decimal; m_ValueType = VT_DOUBLE; };
+		FORCEINLINE void					SetBool( bool boolean ) { SetInt( boolean ? 1 : 0 ); };
 
-		// TODO: Make conversions in runtime from one type to another. E.g interger 10 to string "10".
-		// Perhaps have a "Fast" version of the function and a converting one
-		FORCEINLINE const std::string&	GetString()			const { return m_StringValue; };
-		FORCEINLINE int					GetInt()			const { return m_IntValue; };
-		FORCEINLINE double				GetDouble()			const { return m_DoubleValue; };
-		FORCEINLINE bool				GetBool()			const { return !!GetInt(); };
-		FORCEINLINE ValueType			GetType()			const { return m_ValueType; };
+		const std::string&		GetString();		// GetString uses internal optimization
+		int						GetInt()			const;
+		double					GetDouble()			const;
+		bool					GetBool()			const;
+		ValueType				GetType()			const;
 
-		FORCEINLINE CValue operator=(const CValue& other)
+		FORCEINLINE CValue operator=( const CValue& other )
 		{
-			switch (other.m_ValueType)
+			switch ( other.m_ValueType )
 			{
-				case VT_STRING:
-					SetString( other.m_StringValue );
-					return *this;
-				case VT_INTEGER:
-					SetInt( other.m_IntValue );
-					return *this;
-				case VT_DOUBLE:
-					SetDouble( other.m_DoubleValue );
-					return *this;
-				default:
-					throw "Invalid operation";
+			case VT_STRING:
+				SetString( other.m_StringValue );
+				return *this;
+			case VT_INTEGER:
+				SetInt( other.m_IntValue );
+				return *this;
+			case VT_DOUBLE:
+				SetDouble( other.m_DoubleValue );
+				return *this;
+			default:
+				throw "Invalid operation";
 			}
 		}
 
@@ -64,5 +62,9 @@ namespace Value
 		int					m_IntValue;
 		double				m_DoubleValue;
 		enum ValueType		m_ValueType;
+
+		// Stringcaching
+		double				m_StringDouble;
+		int					m_StringInt;
 	};
 }
--- Shared/Value.cpp
+++ Shared/Value.cpp
@@ -7,6 +7,8 @@ namespace Value
 		m_DoubleValue = 0.0;
 		m_IntValue = 0;
 		m_ValueType = VT_INTEGER;
+		m_StringDouble = 0.0;
+		m_StringInt = 0;
 	}
 
 	CValue::CValue( const CValue& value )
@@ -15,6 +17,8 @@ namespace Value
 		m_DoubleValue = value.m_DoubleValue;
 		m_IntValue = value.m_IntValue;
 		m_StringValue = value.m_StringValue;
+		m_StringDouble = value.m_StringDouble;
+		m_StringInt = value.m_StringInt;
 	}
 
 	CValue::CValue( const std::string& string )
@@ -40,5 +44,82 @@ namespace Value
 	CValue::CValue( bool boolean )
 	{
 		SetBool( boolean );
+	}
+
+	const std::string& CValue::GetString()
+	{
+		switch ( m_ValueType )
+		{
+		case VT_STRING:
+			return m_StringValue;
+		case VT_DOUBLE:
+		{
+			if ( m_StringDouble == m_DoubleValue )
+			{
+				return m_StringValue;
+			}
+			else
+			{
+				m_StringValue = std::to_string( m_DoubleValue );
+				m_StringDouble = m_DoubleValue;
+				return m_StringValue;
+			}
+		}
+		case VT_INTEGER:
+		{
+			if ( m_StringInt == m_IntValue )
+			{
+				return m_StringValue;
+			}
+			else
+			{
+				m_StringValue = std::to_string( m_IntValue );
+				m_StringInt = m_IntValue;
+				return m_StringValue;
+			}
+		}
+		default:
+			throw "Invalid CValue type";
+		}
+	}
+
+	int CValue::GetInt() const
+	{
+		switch ( m_ValueType )
+		{
+		case VT_INTEGER:
+			return m_IntValue;
+		case VT_DOUBLE:
+			return ( int ) m_DoubleValue;
+		case VT_STRING:
+			return m_StringValue.length() > 0;
+		default:
+			throw "Invalid CValue type";
+		}
+	}
+
+	double CValue::GetDouble() const
+	{
+		switch ( m_ValueType )
+		{
+		case VT_DOUBLE:
+			return m_DoubleValue;
+		case VT_INTEGER:
+			return ( double ) m_IntValue;
+		case VT_STRING:
+			return m_StringValue.length() > 0 ? 1.0 : 0.0;
+		default:
+			throw "Invalid CValue type";
+		}
+	}
+
+	bool CValue::GetBool() const
+	{
+		return !!GetInt();
+	}
+
+	ValueType CValue::GetType() const
+	{
+		return m_ValueType;
 	}
 }
