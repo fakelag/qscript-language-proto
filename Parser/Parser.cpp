cfabd886e39f8036d7573343e4f05c90e6be0e84
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-22 17:59:43 +0300

    Add initial exception handling code to parser (WIP)

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -1,6 +1,7 @@
 ﻿#include <functional>
 #include <algorithm>
 #include "Parser.h"
+#include "Exception.h"
 
 namespace Parser
 {
@@ -70,6 +71,12 @@ namespace Parser
 			return m_Symbols[ ++m_CurrentSymbol - 1];
 		}
 
+		void OnException( const Grammar::SymbolLoc_t& location, const std::string& exception )
+		{
+			m_HasErrors = true;
+			m_Exceptions.AddError( location, exception );
+		}
+
 		void AddExpression( AST::IExpression* expression )
 		{
 			m_Expressions.push_back( expression );
@@ -84,6 +91,9 @@ namespace Parser
 		std::vector< AST::IExpression* > 			m_Expressions;
 		std::vector< ParserSymbol_t >				m_Symbols;
 		int 										m_CurrentSymbol;
+
+		bool										m_HasErrors;
+		ParseException								m_Exceptions;
 	};
 
 	std::vector< AST::IExpression* > Parse( const std::vector< Lexer::LexerSymbol_t >& symbols )
@@ -94,119 +104,134 @@ namespace Parser
 		// m_LeftBind and m_RightBind to form a tree like structure
 		auto nextExpression = [ &parserState ]( int rbp = 0 /* Right binding power */ ) -> AST::IExpression*
 		{
-			// Get the current symbol (and advance to the next one)
-			auto curSymbol = parserState.NextSymbol();
-
-			// Construct the left hand tree
-			auto leftSide = curSymbol.m_RightBind( curSymbol );
-
-			// Override -- if we only need the first right hand symbol, return it now
-			if ( rbp == -1 )
-				return leftSide;
-
-			// Finished parsing while expecting more tokens,
-			// this will happen with inputs like "var x = 4 +"
-			// where the last operator is expecting a right hand argument
-			if ( parserState.IsFinished() )
-				throw "Expected end of expression";
-
-			// Call left bind while the current right binding power is less than the
-			// next symbols left binding power. E.g:
-			// 4	+	2	*	2	+	1;
-			//		│		│		└ binding power is 10
-			//		│		│ 
-			//		│		└─ binding power is 20
-			//		└─ binding power is 10
-			//
-			// this will group (2*2) at the bottom of the tree
-			// while (4+(2*2)) is the second outer node,
-			// and (4+(2*2))+1) the most outer node
-			while ( rbp < parserState.CurrentSymbol().m_LBP )
+			AST::IExpression* leftHand = NULL;
+			try
 			{
-				curSymbol = parserState.NextSymbol();
-				leftSide = curSymbol.m_LeftBind( curSymbol, leftSide );
+				// Get the current symbol (and advance to the next one)
+				auto curSymbol = parserState.NextSymbol();
+
+				// Construct the left hand tree
+				leftHand = curSymbol.m_RightBind( curSymbol );
+
+				// Override -- if we only need the first right hand symbol, return it now
+				if ( rbp == -1 )
+					return leftHand;
+
+				// Finished parsing while expecting more tokens,
+				// this will happen with inputs like "var x = 4 +"
+				// where the last operator is expecting a right hand argument
+				if ( parserState.IsFinished() )
+					throw "Expected end of expression";
+
+				// Call left bind while the current right binding power is less than the
+				// next symbols left binding power. E.g:
+				// 4	+	2	*	2	+	1;
+				//		│		│		└ binding power is 10
+				//		│		│ 
+				//		│		└─ binding power is 20
+				//		└─ binding power is 10
+				//
+				// this will group (2*2) at the bottom of the tree
+				// while (4+(2*2)) is the second outer node,
+				// and (4+(2*2))+1) the most outer node
+				while ( rbp < parserState.CurrentSymbol().m_LBP )
+				{
+					curSymbol = parserState.NextSymbol();
+					leftHand = curSymbol.m_LeftBind( curSymbol, leftHand );
+				}
+			}
+			catch ( const ParseException& exception )
+			{
+				parserState.OnException( exception.locations()[ 0 ], exception.errors()[ 0 ] );
 			}
 
-			return leftSide;
+			return leftHand;
 		};
 
 		std::vector< ParserSymbol_t > parserSymbols;
 
-		// Map lexer symbols to parser symbols
-		std::transform( symbols.begin(), symbols.end(), std::back_inserter( parserSymbols ),
-			[ &nextExpression ]( const Lexer::LexerSymbol_t& lexerSymbol ) -> ParserSymbol_t {
-			ParserSymbol_t symbol( lexerSymbol );
+		try
+		{
+			// Map lexer symbols to parser symbols
+			std::transform( symbols.begin(), symbols.end(), std::back_inserter( parserSymbols ),
+				[ &nextExpression ]( const Lexer::LexerSymbol_t& lexerSymbol ) -> ParserSymbol_t {
+				ParserSymbol_t symbol( lexerSymbol );
 
-			switch ( lexerSymbol.m_Symbol )
-			{
-			case Grammar::Symbol::S_INTCNST:
-			{
-				symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+				switch ( lexerSymbol.m_Symbol )
 				{
-					Value::CValue intValue( std::stoi( symbol.m_Token ) );
-					return new AST::CValueExpression( intValue, symbol.m_Symbol, symbol.m_Locations );
-				};
-				break;
-			}
-			case Grammar::Symbol::S_NAME:
-			{
-				symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+				case Grammar::Symbol::S_INTCNST:
 				{
-					Value::CValue intValue( symbol.m_Token );
-					return new AST::CValueExpression( intValue, symbol.m_Symbol, symbol.m_Locations );
-				};
-				break;
-			}
-			case Grammar::Symbol::S_ADD:
-			case Grammar::Symbol::S_SUB:
-			{
-				// Left binding function receives the left hand side expression already parsed for it
-				// and binds it to the tree
-				symbol.m_LeftBind = [ &nextExpression ]( const ParserSymbol_t& symbol, AST::IExpression* left ) -> AST::IExpression*
+					symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+					{
+						Value::CValue intValue( std::stoi( symbol.m_Token ) );
+						return new AST::CValueExpression( intValue, symbol.m_Symbol, symbol.m_Locations );
+					};
+					break;
+				}
+				case Grammar::Symbol::S_NAME:
 				{
-					auto right = nextExpression( symbol.m_LBP );
-					return new AST::CComplexExpression( left, right, symbol.m_Symbol, symbol.m_Locations );
-				};
+					symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+					{
+						Value::CValue intValue( symbol.m_Token );
+						return new AST::CValueExpression( intValue, symbol.m_Symbol, symbol.m_Locations );
+					};
+					break;
+				}
+				case Grammar::Symbol::S_ADD:
+				case Grammar::Symbol::S_SUB:
+				{
+					// Left binding function receives the left hand side expression already parsed for it
+					// and binds it to the tree
+					symbol.m_LeftBind = [ &nextExpression ]( const ParserSymbol_t& symbol, AST::IExpression* left ) -> AST::IExpression*
+					{
+						auto right = nextExpression( symbol.m_LBP );
+						return new AST::CComplexExpression( left, right, symbol.m_Symbol, symbol.m_Locations );
+					};
 
-				// Right binding function only parses ahead of the current token. Usually to embed the next expression to
-				// it's tree
-				symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+					// Right binding function only parses ahead of the current token. Usually to embed the next expression to
+					// it's tree
+					symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+					{
+						auto right = nextExpression( -1 );
+						return new AST::CSimpleExpression( right, symbol.m_Symbol, symbol.m_Locations );
+					};
+					break;
+				}
+				case Grammar::Symbol::S_MUL:
+				case Grammar::Symbol::S_DIV:
+				case Grammar::Symbol::S_POW:
+				case Grammar::Symbol::S_MOD:
+				case Grammar::Symbol::S_ASSIGN:
 				{
-					auto right = nextExpression( -1 );
-					return new AST::CSimpleExpression( right, symbol.m_Symbol, symbol.m_Locations );
-				};
-				break;
-			}
-			case Grammar::Symbol::S_MUL:
-			case Grammar::Symbol::S_DIV:
-			case Grammar::Symbol::S_POW:
-			case Grammar::Symbol::S_MOD:
-			case Grammar::Symbol::S_ASSIGN:
-			{
-				symbol.m_LeftBind = [ &nextExpression ]( const ParserSymbol_t& symbol, AST::IExpression* left ) -> AST::IExpression*
+					symbol.m_LeftBind = [ &nextExpression ]( const ParserSymbol_t& symbol, AST::IExpression* left ) -> AST::IExpression*
+					{
+						auto right = nextExpression( symbol.m_LBP );
+						return new AST::CComplexExpression( left, right, symbol.m_Symbol, symbol.m_Locations );
+					};
+					break;
+				}
+				case Grammar::Symbol::S_VAR:
 				{
-					auto right = nextExpression( symbol.m_LBP );
-					return new AST::CComplexExpression( left, right, symbol.m_Symbol, symbol.m_Locations );
-				};
-				break;
-			}
-			case Grammar::Symbol::S_VAR:
-			{
-				symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
-				{
-					auto nameExpr = nextExpression( symbol.m_LBP );
-					return new AST::CSimpleExpression( nameExpr, symbol.m_Symbol, symbol.m_Locations );
-				};
-				break;
-			}
-			case Grammar::Symbol::S_SEMICOLON:
-				break;
-			default:
-				throw "Unhandled symbol";
-			}
+					symbol.m_RightBind = [ &nextExpression ]( const ParserSymbol_t& symbol ) -> AST::IExpression*
+					{
+						auto nameExpr = nextExpression( symbol.m_LBP );
+						return new AST::CSimpleExpression( nameExpr, symbol.m_Symbol, symbol.m_Locations );
+					};
+					break;
+				}
+				case Grammar::Symbol::S_SEMICOLON:
+					break;
+				default:
+					throw ParseException( lexerSymbol.m_Locations, "Unknown symbol: " + std::to_string( lexerSymbol.m_Symbol ) );
+				}
 
-			return symbol;
-		} );
+				return symbol;
+			} );
+		}
+		catch ( const ParseException& exception )
+		{
+			parserState.OnException( exception.locations()[ 0 ], exception.errors()[ 0 ] );
+		}
 
 		// Load the symbols
 		parserState.AddSymbols( parserSymbols );
@@ -217,6 +242,8 @@ namespace Parser
 			parserState.AddExpression( nextExpression() );
 			parserState.NextSymbol();
 		}
+
+		if ( parserState.Exceptions() ) // todo: this
 
 		return parserState.Expressions();
 	}
