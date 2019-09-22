16fce0aa5f20fa6618dbe87e4b51f1a7c1c62380
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-22 17:59:21 +0300

    Add exceptions classes

---
+++ Includes/Exception.h
@@ -0,0 +1,50 @@
+#include <exception>
+#include <string>
+#include <vector>
+#include "Grammar.h"
+
+class Exception : public std::exception
+{
+public:
+	Exception( const std::string& what )
+	{
+		m_What = what;
+	}
+
+	const char* what() const { return m_What.c_str(); }
+private:
+	std::string m_What;
+};
+
+class ParseException : public Exception
+{
+public:
+	ParseException() : Exception( "Parsing exception" )
+	{
+	}
+
+	ParseException( const std::vector<Grammar::SymbolLoc_t> locations, const std::vector<std::string> errors )
+		: Exception( "Parsing exception" )
+	{
+		m_Locations = locations;
+		m_Errors = errors;
+	}
+
+	ParseException( const Grammar::SymbolLoc_t& location, const std::string& error )
+		: Exception( "Parsing exception" )
+	{
+		AddError( location, error );
+	}
+
+	void AddError( const Grammar::SymbolLoc_t& location, const std::string& error )
+	{
+		m_Locations.push_back( location );
+		m_Errors.push_back( error );
+	}
+
+	const std::vector< Grammar::SymbolLoc_t >& locations() const { return m_Locations; }
+	const std::vector< std::string >& errors() const { return m_Errors; }
+private:
+	std::vector< Grammar::SymbolLoc_t >		m_Locations;
+	std::vector< std::string >				m_Errors;
+};
