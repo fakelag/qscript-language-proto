528e32c066b82cf05a3659085bc34df6160e5c4c
fakelag <35497506+fakelag@users.noreply.github.com>
2019-09-30 18:34:04 +0300

    Add preliminary memory tests

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -3,10 +3,27 @@
 #include "windows.h"
 #endif
 
+#include "Memory.h"
 #include "UnitTests.h"
 #include "Tests.h"
 
-int main()
+#include "Lexer.h"
+#include "Parser.h"
+#include "AST.h"
+
+bool HasArg( const std::string& argument, int argc, const char** argv )
+{
+	// Iterate through all arguments -- the first one is the program path
+	for ( int i = 1; i < argc; ++i )
+	{
+		if ( argument == argv[ i ] )
+			return true;
+	}
+
+	return false;
+}
+
+int main( int argc, const char** argv )
 {
 #if defined(_WIN32) || defined(_WIN64)
 	// Enable ANSI colors for windows console
@@ -17,10 +34,40 @@ int main()
 	SetConsoleMode( hStdOut, dwMode );
 #endif
 
-	// Call tests
-	RunLexerTests();
-	RunValueTests();
-	RunParserTests();
+	if ( HasArg( "-memtest", argc, argv ) )
+	{
+		size_t beginUsage = Memory::GetVirtualMemoryUsage();
+		size_t finishUsage = 0;
+
+		std::cout << "Running memory tests.." << std::endl;
+		std::cout << "Current memory usage: " << Memory::FormatBytes( beginUsage ) << std::endl;
+
+		std::vector< AST::IExpression* > allExpressions;
+
+		for ( int i = 0; i < 10000 * 8; ++i )
+		{
+			auto lexed = Lexer::Parse( "{ a = 1 + 2 * 2 + -5; a = a || 8; } { a; b; a++ || b; }" );
+			// AST::FreeTree( Parser::Parse( lexed ) );
+
+			auto expressions = Parser::Parse( lexed );
+			allExpressions.insert( allExpressions.end(), expressions.begin(), expressions.end() );
+		}
+
+		AST::FreeTree( allExpressions );
+		allExpressions.clear();
+
+		finishUsage = Memory::GetVirtualMemoryUsage();
+
+		std::cout << "Memory usage when finished: " << Memory::FormatBytes( finishUsage ) << std::endl;
+		std::cout << "Difference: " << Memory::FormatBytes( finishUsage - beginUsage ) << std::endl;
+	}
+	else
+	{
+		// Call tests
+		RunLexerTests();
+		RunValueTests();
+		RunParserTests();
+	}
 
 	std::getchar();
 	return 0;
