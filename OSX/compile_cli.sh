ac70af9da0a605361db0e884b538fe58c037f7ee
fakelag <35497506+fakelag@users.noreply.github.com>
2019-10-22 19:17:49 +0300

    Add OSX runtime compilation script

--- OSX/compile_cli.sh
+++ OSX/compile_cli.sh
@@ -1,3 +1,4 @@
 sh compile_lexer.sh
 sh compile_parser.sh
-g++ -I ../Includes/ ../CLI/Main.cpp ./Lib/Lexer.a ./Lib/Parser.a ./Lib/Value.a ./Lib/AST.a -Wc++11-extensions -std=c++11 -o ./lib/CLI.o -D _OSX
+sh compile_runtime.sh
+g++ -I ../Includes/ ../CLI/Main.cpp ./Lib/Lexer.a ./Lib/Parser.a ./Lib/Value.a ./Lib/AST.a ./Lib/Runtime/Rt.a -Wc++11-extensions -std=c++11 -o ./lib/CLI.o -D _OSX
