2a8dc8aacbef53551613077d8e707b2f56616227
fakelag <35497506+fakelag@users.noreply.github.com>
2019-10-23 19:25:30 +0300

    Add initial runtime tests

---
+++ UnitTests/RuntimeTests.cpp
@@ -0,0 +1,41 @@
+#include "UnitTests.h"
+#include "Tests.h"
+
+#include "Lexer.h"
+#include "Parser.h"
+#include "Runtime.h"
+
+void RunRuntimeTests()
+{
+	UTEST_BEGIN( "Parser Tests" );
+
+	AST::PushTrackAlloc();
+
+	UTEST_CASE( "Simple math" )
+	{
+		auto syntaxTree = Parser::Parse( Lexer::Parse( "\
+			1 + 1; 										\
+			1 + (2 + 2) + 5;							\
+		" ) );
+
+		auto context = Runtime::CreateDefaultContext( true );
+		auto results = Runtime::Execute( syntaxTree, context );
+
+		UTEST_ASSERT( results.size() == 2 );
+		UTEST_ASSERT( results[ 0 ].m_Value == Value::CValue( 2 ) );
+		UTEST_ASSERT( results[ 1 ].m_Value == Value::CValue( 10 ) );
+
+		AST::FreeTree( syntaxTree );
+		UTEST_CASE_CLOSED();
+	}( );
+
+	UTEST_CASE( "Parser/AST memory management" )
+	{
+		auto leakedNodes = AST::AllocatedExpressions();
+		UTEST_ASSERT( leakedNodes.size() == 0 );
+		UTEST_CASE_CLOSED();
+	}( );
+
+	AST::PopAllocTracking();
+	UTEST_END();
+}
