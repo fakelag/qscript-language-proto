65a19e213e78bbc4160097712182448400bab8be
fakelag <35497506+fakelag@users.noreply.github.com>
2019-10-24 19:45:48 +0300

    Add tests for arithmetic operators (runtime)

--- UnitTests/RuntimeTests.cpp
+++ UnitTests/RuntimeTests.cpp
@@ -52,6 +52,34 @@ void RunRuntimeTests()
 		UTEST_CASE_CLOSED();
 	}( );
 
+	UTEST_CASE( "Basic arithmetics (Order of operations)" )
+	{
+		auto syntaxTree = Parser::Parse( Lexer::Parse( "\
+			(18 - 3) * 10 + 2;																					\
+			6 * 6 + 7 * 5;																						\
+			146 + 7 * 14 - 31;																					\
+			4 * 2 + -(40 / 20) * 3;																				\
+			-66 - 30 / 10 * (4 / 2) + 5;																		\
+			-(3 * ((2 + -4) - 4) / -1 * (((-(3 - 2) + ((2) -2) - 4) * 2)) * 2 + -(6 / (1 + 1 ) - 2 * 2)); 		\
+		" ) );
+
+		Runtime::CContext context;
+		Runtime::CreateDefaultContext( true, true, &context );
+
+		auto results = Runtime::Execute( syntaxTree, context );
+
+		UTEST_ASSERT( results.size() == 6 );
+		UTEST_ASSERT( results[ 0 ].m_Value == Value::CValue( (18 - 3) * 10 + 2 ) );
+		UTEST_ASSERT( results[ 1 ].m_Value == Value::CValue( 6 * 6 + 7 * 5 ) );
+		UTEST_ASSERT( results[ 2 ].m_Value == Value::CValue( 146 + 7 * 14 - 31 ) );
+		UTEST_ASSERT( results[ 3 ].m_Value == Value::CValue( 4 * 2 + -(40 / 20) * 3 ) );
+		UTEST_ASSERT( results[ 4 ].m_Value == Value::CValue( -66 - 30 / 10 * (4 / 2) + 5 ) );
+		UTEST_ASSERT( results[ 5 ].m_Value == Value::CValue( -(3 * ((2 + -4) - 4) / -1 * (((-(3 - 2) + ((2) -2) - 4) * 2)) * 2 + -(6 / (1 + 1 ) - 2 * 2)) ) );
+
+		AST::FreeTree( syntaxTree );
+		UTEST_CASE_CLOSED();
+	}( );
+
 	UTEST_CASE( "Parser/AST memory management" )
 	{
 		auto leakedNodes = AST::AllocatedExpressions();
