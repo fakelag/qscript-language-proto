3f10e356b6dc8bbc57134a2d5ab8f87ebb9ba09b
fakelag <35497506+fakelag@users.noreply.github.com>
2019-10-27 10:42:56 +0200

    Add autoresolving variables to Name runtime operator

--- Runtime/Core/Name.cpp
+++ Runtime/Core/Name.cpp
@@ -3,5 +3,15 @@
 
 RTI_EXECFN_VALUE( S_NAME )
 {
-	return { m_Value };
+	if ( context.m_ExecFlags & Runtime::CContext::CF_NORESOLVE )
+		return { m_Value };
+
+	if ( m_Value.GetType() != Value::ValueType::VT_STRING )
+		throw RuntimeException( m_Loc, "Invalid variable name: " + m_Value.GetString() );
+
+	Value::CValue variable;
+	if ( !context.FindVariable( m_Value.GetString(), &variable ) )
+		throw RuntimeException( m_Loc, "Variable \"" + m_Value.GetString() + "\" is not defined" );
+
+	return { variable };
 }
