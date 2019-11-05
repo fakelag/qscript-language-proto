3bb04b4d5b6898733f600ebffa9d891d157db074
fakelag <35497506+fakelag@users.noreply.github.com>
2019-11-05 19:23:07 +0200

    Add additive assignment operators runtime

---
+++ Runtime/Core/AssignSub.cpp
@@ -0,0 +1,22 @@
+#include "Runtime.h"
+#include "RuntimeInternal.h"
+
+RTI_EXECFN_COMPLEX( S_ASSIGN_SUB )
+{
+	context.AddFlag( Runtime::CContext::CF_NORESOLVE );
+	auto varName = m_LHS->Execute( context );
+	context.RemoveFlag( Runtime::CContext::CF_NORESOLVE );
+
+	auto newValue = m_RHS->Execute( context );
+
+	if ( varName.m_Value.GetType() != Value::ValueType::VT_STRING )
+		throw RuntimeException( m_Loc, "Invalid variable name: " + varName.m_Value.GetString() );
+
+	auto variable = context.FindVariable( varName.m_Value.GetString() );
+
+	if ( !variable )
+		throw RuntimeException( m_Loc, "Variable \"" + varName.m_Value.GetString() + "\" is not defined" );
+
+	*variable = *variable - newValue.m_Value;
+	return { *variable };
+}
